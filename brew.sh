a6bb529dc8f58ae130c477bf61c35a439f180d42
Anton Franchuk <afranchuk@blackberry.com>
2022-08-03 17:48:46 +0300

    Add bash script to install all homebrew packages

---
+++ brew.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Install Brew Packages
+
+brew install node
+brew install nvm
+
+brew install --cask adoptopenjdk/openjdk/adoptopenjdk11
+brew install --cask adoptopenjdk/openjdk/adoptopenjdk13
+brew install --cask adoptopenjdk8
+
+brew install --cask amethyst
+brew install bottom
+brew install coreutils
+brew install --cask iterm2
+brew install lf
+brew install make
+brew install --cask postman
+brew install python
+brew install source-highlight
+brew install watchman
+brew install yarn
+
+brew install --cask google-chrome
+brew tap homebrew/cask-versions && brew install --cask google-chrome-canary
+
+brew install --cask android-studio
+brew install --cask visual-studio-code
+brew install neovim
+
+brew install --cask slack
+brew install --cask zoom
